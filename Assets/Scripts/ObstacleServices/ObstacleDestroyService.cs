49325eafcef83a8de509754887a7e77dddc94b21
Drowpall <drowpall@gmail.com>
2021-08-25 02:56:47 +0300

    Fix class naming typo

--- Assets/Scripts/ObstacleServices/ObstacleDestroyService.cs
+++ Assets/Scripts/ObstacleServices/ObstacleDestroyService.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObstacleDestroyer : MonoBehaviour
+public class ObstacleDestroyService : MonoBehaviour
 {
     public GameObject obstacle;
 
