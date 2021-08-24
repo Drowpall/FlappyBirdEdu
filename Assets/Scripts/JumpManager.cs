0f1340eca0ed65af98c3ba000c9263e250f44371
Drowpall <drowpall@gmail.com>
2021-08-24 20:00:30 +0300

    Simplify if statement

--- Assets/Scripts/JumpManager.cs
+++ Assets/Scripts/JumpManager.cs
@@ -55,7 +55,7 @@ public class JumpManager : MonoBehaviour
 
     void FixedUpdate()
     {
-        if(isSpaceKeyPressed == true)
+        if(isSpaceKeyPressed)
         {
             DisplayVelocity();
             ApplyVelocityAlteration();
