9d783a5e44db63dd81246f22dd3f677a9ae2c2c8
Drowpall <drowpall@gmail.com>
2021-09-27 13:21:00 +0300

    Add basic game over UI

--- Assets/Scripts/Misc/LevelController.cs
+++ Assets/Scripts/Misc/LevelController.cs
@@ -2,19 +2,29 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelController : MonoBehaviour {
     [SerializeField] Player player;
     [SerializeField] Spawner spawner;
-
-    void Start() {
+    [SerializeField] UIController uiController;
+    void Start() 
+    {
         player.Died += OnPlayerDied;
     }
 
-    void OnPlayerDied() {
+    void OnPlayerDied() 
+    {
         spawner.enabled = false;
         foreach (var obstacle in FindObjectsOfType<Obstacle>()) {
             obstacle.enabled = false;
         }
+        uiController.PlayDeathSceneUI();
     }
+
+    public void Restart()
+    {
+       SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
 }
--- Assets/Scripts/Misc/UIController.cs
+++ Assets/Scripts/Misc/UIController.cs
@@ -6,10 +6,20 @@ using UnityEngine.UI;
 public class UIController : MonoBehaviour
 {
     [SerializeField] Text score;
+    [SerializeField] Text gameOver;
     [SerializeField] Player player;
+    [SerializeField] Button restartButton;
 
     void Update()
     {
         score.text = player.Score.ToString();
+    }
+
+    public void PlayDeathSceneUI()
+    {
+        restartButton.gameObject.SetActive(true);
+        score.gameObject.SetActive(false);
+        gameOver.text = "Game Over" + "\nYour score is: " + player.Score.ToString();
+        gameOver.gameObject.SetActive(true);
     }
 }
